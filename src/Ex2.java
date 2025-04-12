278394af82518068d563f9f54a88ef7de40d7426
moka <day177891@gmail.com>
2025-04-12 20:32:03 +0900

    test

---
+++ src/Ex2.java
@@ -0,0 +1,28 @@
+public class Ex2 {
+    public class Ex11 {
+        public static void main(String[] args) {
+            String value = "12o34";
+            char ch = ' ';
+            boolean isNumber = true;
+
+            for (int i = 0; i < value.length(); i++) {
+                ch = value.charAt(i);
+                if (ch < '0' || ch > '9') {
+                    isNumber = false;
+                    break;
+                }
+            }
+
+            if (isNumber) {
+                System.out.println(value + "는 숫자입니다.");
+            } else {
+                System.out.println(value + "는 숫자가 아닙니다.");
+            }
+        }
+    }
+
+
+
+
+
+}
